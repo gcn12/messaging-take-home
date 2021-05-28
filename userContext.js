02ac90359361390d6b8f5b4f4fba69fa7e70d798
Gareth Ng <garethcng@gmail.com>
2021-05-28 15:06:20 -0400

    added more items to context api state

--- userContext.js
+++
@@ -1,8 +0,0 @@
-import { createContext } from 'react'
-
-const AppContext = createContext()
-
-export const AppProvider = AppContext.Provider
-export const AppConsumer = AppContext.Consumer
-
-export default AppContext
